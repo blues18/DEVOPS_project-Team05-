a93c9ca9a58d2bdcdeeb529d4c948113b34b2b95
KEVINLOVEFOOD <2104627A@student.tp.edu.sg>
2022-11-29 23:39:32 +0800

    Added dependencies inside POM file Created User.Java Created UserServlet.Java Created userManagement.jsp

---
+++ DevOps_project/src/main/java/com/dvops/maven/eclipse/User.java
@@ -0,0 +1,43 @@
+package com.dvops.maven.eclipse;
+
+public class User {
+	
+	public User(String name, String password, String email, String races) {
+		super();
+		this.name = name;
+		this.password = password;
+		this.email = email;
+		this.races = races;
+	}
+	
+	public String getName() {
+		return name;
+	}
+	public void setName(String name) {
+		this.name = name;
+	}
+	public String getPassword() {
+		return password;
+	}
+	public void setPassword(String password) {
+		this.password = password;
+	}
+	public String getEmail() {
+		return email;
+	}
+	public void setEmail(String email) {
+		this.email = email;
+	}
+	public String getRaces() {
+		return races;
+	}
+	public void setRaces(String races) {
+		this.races = races;
+	}
+	
+	protected String name;
+	protected String password;
+	protected String email;
+	protected String races;
+
+}
