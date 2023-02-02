36ab5fbcaa23a030388878f73ea1dfbdb0939316
RyuukoShiro <ZRyuukoX@gmail.com>
2023-02-02 19:20:13 +0800

    Test: testing jenkins
    test

---
+++ DevOps_project/src/test/java/com/dvops/maven/eclipse/TableServletTest.java
@@ -0,0 +1,29 @@
+package com.dvops.maven.eclipse;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+class TableServletTest {
+
+	@BeforeEach
+	void setUp() throws Exception {
+	}
+
+	@AfterEach
+	void tearDown() throws Exception {
+	}
+
+	@Test
+	void testDoGetHttpServletRequestHttpServletResponse() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	void testDoPostHttpServletRequestHttpServletResponse() {
+		fail("Not yet implemented");
+	}
+
+}
